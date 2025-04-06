0403487ad521f44d0b99c7d033dba51b77857c3e
RavenHu001 <2419652744@qq.com>
2025-04-05 19:13:44 -0700

    Add map active, and add file to store GoogleAPI

--- CityClean/app/src/main/java/com/example/cityclean/MainActivity.java
+++ CityClean/app/src/main/java/com/example/cityclean/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.cityclean;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +10,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button goMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +21,8 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        goMap = findViewById(R.id.GoMap);
+
     }
 }

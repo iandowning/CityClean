1872dd10e20fb2d0d678687f8f8c5e0d7cbd303e
RavenHu001 <2419652744@qq.com>
2025-04-07 14:49:59 -0700

    add adapter and item row file

---
+++ CityClean/app/src/main/java/com/example/cityclean/MyAdapter.java
@@ -0,0 +1,75 @@
+package com.example.cityclean;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private List<Report> itemList;
+    private Context context;
+
+    public MyAdapter(Context context, List<Report> itemList) {
+        this.context = context;
+        this.itemList = itemList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_row, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Report item = itemList.get(position);
+        holder.date.setText(item.getDate());
+
+        // 设置图片
+        int imageResId = context.getResources().getIdentifier(item.getImagePath(), "drawable", context.getPackageName());
+        holder.image.setImageResource(imageResId);
+
+        // 点击事件
+        holder.itemView.setOnClickListener(v -> {
+            Intent intent = new Intent(context, ReportDetial.class);
+            intent.putExtra("id", item.getId());
+            context.startActivity(intent);
+        });
+
+        // 删除按钮
+        holder.deleteButton.setOnClickListener(v -> {
+            itemList.remove(position);
+            notifyItemRemoved(position);
+            Toast.makeText(context, "Item deleted", Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView date;
+        ImageView image;
+        Button deleteButton;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            date = itemView.findViewById(R.id.item_date);
+            image = itemView.findViewById(R.id.item_image);
+            deleteButton = itemView.findViewById(R.id.item_delete);
+        }
+    }
+}
+
